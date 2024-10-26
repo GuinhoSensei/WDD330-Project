7c2f77a87ea74d4e88a741b04053ffde96bf52e2
GuinhoSensei <thiggouveia@hotmail.com>
2024-10-26 15:00:13 -0700

    Weather App

--- js/animations.js
+++
@@ -1,7 +0,0 @@
-// animations.js
-export function animateWeather() {
-    const weatherDiv = document.getElementById('current-weather');
-    weatherDiv.classList.add('fade-in');
-}
-
-// Add CSS for the animation
